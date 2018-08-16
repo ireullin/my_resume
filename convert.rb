652abf3307ee15a5a358b226d185599b83d88863
ireullin <cheinheng.lin@uitox.com>
2018-08-16 17:27:23 +0800

    An available version without server side

--- convert.rb
+++ convert.rb
@@ -2,7 +2,10 @@ require 'yaml'
 
 def main
 
-    p YAML.load_file("resume.yaml")
+    data = YAML.load_file("resume.yaml")
+    data['attainments']['items'].each do |r|
+        p r['summary']['zh']
+    end
 end
 
 main if $0==__FILE__
